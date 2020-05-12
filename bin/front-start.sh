50debbb18977798bc43f1bf09172ce80bb26209f
kangtiancheng <kangtiancheng001@163.com>
2020-05-12 18:30:16 +0800

    绝对路径

--- bin/front-start.sh
+++ bin/front-start.sh
@@ -4,6 +4,6 @@ git pull
 # 执行打包
 mvn clean package -Pprod
 
-java -jar ../novel-front/target/novel-front-1.1.0.jar --spring.profiles.active=prod  &
+java -jar /app/ly/server/docker/ly_novel/novel-front/target/novel-front-1.1.0.jar --spring.profiles.active=prod  &
 
 echo "novel-front部署完毕，Enjoy！"
