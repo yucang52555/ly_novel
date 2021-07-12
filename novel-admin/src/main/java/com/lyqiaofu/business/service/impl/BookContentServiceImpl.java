fed456019ffd65988223b4c270d2c1c896901860
樵夫 <kangtiancheng001@163.com>
2021-07-12 17:44:04 +0800

    章节内容绑定章节信息删除

---
+++ novel-admin/src/main/java/com/lyqiaofu/business/service/impl/BookContentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.lyqiaofu.business.service.impl;
+
+import com.lyqiaofu.business.dao.BookCommentDao;
+import com.lyqiaofu.business.dao.BookContentDao;
+import com.lyqiaofu.business.domain.BookCommentDO;
+import com.lyqiaofu.business.domain.BookContentDO;
+import com.lyqiaofu.business.service.BookCommentService;
+import com.lyqiaofu.business.service.BookContentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class BookContentServiceImpl implements BookContentService {
+
+    @Autowired
+    BookContentDao bookContentDao;
+
+    @Override
+    public int remove(String indexId) {
+        return bookContentDao.remove(indexId);
+    }
+}
--- novel-crawl/src/main/java/com/lyqiaofu/novel/controller/CrawlController.java
+++ novel-crawl/src/main/java/com/lyqiaofu/novel/controller/CrawlController.java
@@ -67,7 +67,4 @@ public class CrawlController {
         return ResultBean.ok();
     }
 
-
-
-
 }
